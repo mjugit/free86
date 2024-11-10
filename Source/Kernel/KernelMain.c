8d0c3d857dc378a41b5ad51d74916d6fc318b42c
Maximilian Jung <maximilian.jung@thevco.org>
2024-11-10 21:33:43 +0000

    Enable VGA test image in kernel main

--- Source/Kernel/KernelMain.c
+++ Source/Kernel/KernelMain.c
@@ -11,11 +11,11 @@ void KernelMain(void) {
   INT_EnableInterrupts();
 
   char buffer[100];
-  short value = 4321;
+  short value = 24321;
   String_Format(buffer, "%dw", value);
   VGA_DrawString(0, 0, buffer, White);
   
-  //VGA_DrawTestImage();
+  VGA_DrawTestImage();
   
   // halt forever
   while (1) __asm__ __volatile__("hlt");
