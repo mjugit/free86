3b0d7b5b03e4000b34159cf92fa8b0064ea8972e
Maximilian Jung <maximilian.jung@thevco.org>
2024-11-16 20:20:40 +0100

    Add simple test code for the new keyboard driver

--- Source/Kernel/KernelMain.c
+++ Source/Kernel/KernelMain.c
@@ -1,22 +1,49 @@
 #include "Include/VgaGraphics.h"
 #include "Include/Interrupt.h"
+#include "Include/Keyboard.h"
 
 #include "../StandardLib/Include/String.h"
+#include "../StandardLib/Include/Stream.h"
 
 void KernelMain(void) {
   VGA_InitializeColorPalette();
 
   INT_InitializeIDT();
   INT_InitializePIC(0xfd, 0xff);
+  Stream_Initialize(&InputStream);
   INT_EnableInterrupts();
 
-  char buffer[100];
-  short value = 24321;
-  String_Format(buffer, "%dw", value);
-  VGA_DrawString(0, 0, buffer, White);
-  
-  VGA_DrawTestImage();
-  
+  // VGA_DrawTestImage();
+  short posx = 0;
+  short posy = 0;
+
+  Charset ger = Keyboard_GetGermanCharset();
+  Stream input;
+
+  Stream_Initialize(&input);
+  Keyboard_SetInputStream(&input);
+  Keyboard_SetCharset(&ger);
+
+  char c = 0;
+  do {
+    if (!Stream_Read(&input, &c))
+      continue;
+
+    VGA_DrawChar(posx, posy, c, White);
+    if (c == '\b') {
+      posx -= 8;
+      continue;
+    }
+    
+    posx += 8;
+    if (posx >= 640 || c == '\n') {
+      posy += 8;
+      posx = 0;
+    }
+  } while (true);
+
+
+
   // halt forever
   while (1) __asm__ __volatile__("hlt");
   
