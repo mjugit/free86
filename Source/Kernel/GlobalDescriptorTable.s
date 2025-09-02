f034930a00020a0e4e34e76762636d17509e507b
Maximilian Jung <maximilian.jung@thevco.org>
2025-09-02 18:04:12 +0000

    Fix segment flags in GDT

--- Source/Kernel/GlobalDescriptorTable.s
+++ Source/Kernel/GlobalDescriptorTable.s
@@ -44,21 +44,21 @@ GlobalDescriptorTable_Head:
 
 	// Kernel code segment
 
-	.word 0x1fff		// Segment limit (8K - 1)
+	.word 0xffff		// Segment limit
 	.word 0x0000		// Base address (low)
 	.byte 0x00 		// Base address (middle)
 	.byte 0x9a		// Access byte (Executable, Readable, Code-Segment)
-	.byte 0xcf		// Flags
+	.byte 0x4f		// Flags
 	.byte 0x00		// Base address (high)
 
 
 	// Kernel data segment
 
-	.word 0xffff		// Segment limit (1024K - 1)
+	.word 0xffff		// Segment limit
 	.word 0x0000 		// Base address (low)
 	.byte 0x00		// Base address (middle)
 	.byte 0x92		// Access byte (Readable, Writable, Data-Segment)
-	.byte 0xcf		// Flags
+	.byte 0x4f		// Flags
 	.byte 0x00 		// Base address (high)
 
 
