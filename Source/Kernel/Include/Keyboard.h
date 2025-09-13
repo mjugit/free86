b1dc2c580b35ccc55225661bb6d5618c7f96a144
Maximilian Jung <maximilian.jung@thevco.org>
2025-09-13 15:39:19 +0000

    Add existing keyboard driver implementation

--- Source/Kernel/Include/Keyboard.h
+++ Source/Kernel/Include/Keyboard.h
@@ -30,224 +30,271 @@
 #ifndef _KEYBOARD_H_
 #define _KEYBOARD_H_
 
+#include "../../Modules/Include/SystemCore.h"
 
-// Enumeration of IBM AT keyboard scan codes.
+
+// An abstract number describing the key
+typedef U8 KeyCode;
+
+
+// Describes the active key modifiers
 typedef enum {
-    ScanCodeEsc          = 0x01,
-    ScanCode1            = 0x02,
-    ScanCode2            = 0x03,
-    ScanCode3            = 0x04,
-    ScanCode4            = 0x05,
-    ScanCode5            = 0x06,
-    ScanCode6            = 0x07,
-    ScanCode7            = 0x08,
-    ScanCode8            = 0x09,
-    ScanCode9            = 0x0A,
-    ScanCode0            = 0x0B,
-    ScanCodeMinus        = 0x0C,
-    ScanCodeEquals       = 0x0D,
-    ScanCodeBackspace    = 0x0E,
-    ScanCodeTab          = 0x0F,
-    ScanCodeQ            = 0x10,
-    ScanCodeW            = 0x11,
-    ScanCodeE            = 0x12,
-    ScanCodeR            = 0x13,
-    ScanCodeT            = 0x14,
-    ScanCodeY            = 0x15,
-    ScanCodeU            = 0x16,
-    ScanCodeI            = 0x17,
-    ScanCodeO            = 0x18,
-    ScanCodeP            = 0x19,
-    ScanCodeLBracket     = 0x1A,
-    ScanCodeRBracket     = 0x1B,
-    ScanCodeEnter        = 0x1C,
-    ScanCodeLCtrl        = 0x1D,
-    ScanCodeA            = 0x1E,
-    ScanCodeS            = 0x1F,
-    ScanCodeD            = 0x20,
-    ScanCodeF            = 0x21,
-    ScanCodeG            = 0x22,
-    ScanCodeH            = 0x23,
-    ScanCodeJ            = 0x24,
-    ScanCodeK            = 0x25,
-    ScanCodeL            = 0x26,
-    ScanCodeSemicolon    = 0x27,
-    ScanCodeApostrophe   = 0x28,
-    ScanCodeGrave        = 0x29,
-    ScanCodeLShift       = 0x2A,
-    ScanCodeBackslash    = 0x2B,
-    ScanCodeZ            = 0x2C,
-    ScanCodeX            = 0x2D,
-    ScanCodeC            = 0x2E,
-    ScanCodeV            = 0x2F,
-    ScanCodeB            = 0x30,
-    ScanCodeN            = 0x31,
-    ScanCodeM            = 0x32,
-    ScanCodeComma        = 0x33,
-    ScanCodePeriod       = 0x34,
-    ScanCodeSlash        = 0x35,
-    ScanCodeRShift       = 0x36,
-    ScanCodeKpAsterisk   = 0x37,
-    ScanCodeLAlt         = 0x38,
-    ScanCodeSpace        = 0x39,
-    ScanCodeCapsLock     = 0x3A,
-    ScanCodeF1           = 0x3B,
-    ScanCodeF2           = 0x3C,
-    ScanCodeF3           = 0x3D,
-    ScanCodeF4           = 0x3E,
-    ScanCodeF5           = 0x3F,
-    ScanCodeF6           = 0x40,
-    ScanCodeF7           = 0x41,
-    ScanCodeF8           = 0x42,
-    ScanCodeF9           = 0x43,
-    ScanCodeF10          = 0x44,
-    ScanCodeNumLock      = 0x45,
-    ScanCodeScrollLock   = 0x46,
-    ScanCodeKp7          = 0x47,
-    ScanCodeKp8          = 0x48,
-    ScanCodeKp9          = 0x49,
-    ScanCodeKpMinus      = 0x4A,
-    ScanCodeKp4          = 0x4B,
-    ScanCodeKp5          = 0x4C,
-    ScanCodeKp6          = 0x4D,
-    ScanCodeKpPlus       = 0x4E,
-    ScanCodeKp1          = 0x4F,
-    ScanCodeKp2          = 0x50,
-    ScanCodeKp3          = 0x51,
-    ScanCodeKp0          = 0x52,
-    ScanCodeKpPeriod     = 0x53,
-    ScanCodeF11          = 0x57,
-    ScanCodeF12          = 0x58,
+  None = 0,
+  Shift = 1 <<0,
+  Ctrl = 1 << 1,
+  Alt = 1 << 2
+} KeyModifiers;
 
-    // Extended scancodes
-    ScanCodeRCtrl        = 0xE01D,
-    ScanCodeRAlt         = 0xE038,
-    ScanCodeHome         = 0xE047,
-    ScanCodeUp           = 0xE048,
-    ScanCodePageUp       = 0xE049,
-    ScanCodeLeft         = 0xE04B,
-    ScanCodeRight        = 0xE04D,
-    ScanCodeEnd          = 0xE04F,
-    ScanCodeDown         = 0xE050,
-    ScanCodePageDown     = 0xE051,
-    ScanCodeInsert       = 0xE052,
-    ScanCodeDelete       = 0xE053
-} ScanCode;
+
+typedef struct KeyEventArgs {
+  // The active key modifiers
+  KeyModifiers* Modifiers;
+
+  // The key code
+  KeyCode KeyCode;
+
+  // Indicates whether the key was pressed or released
+  bool WasKeyPress;
+} KeyEventArgs;
+
+
+typedef struct KeyMapEntry {
+  char Normal;
+  char Shift;
+  char Ctrl;
+  char Alt;
+} KeyMapEntry;
+
+
+typedef void (*KeyEventHandler)(KeyEventArgs args);
+
+
+
+// Scan codes
+#define SCANCODE_ESC          0x01
+#define SCANCODE_1            0x02
+#define SCANCODE_2            0x03
+#define SCANCODE_3            0x04
+#define SCANCODE_4            0x05
+#define SCANCODE_5            0x06
+#define SCANCODE_6            0x07
+#define SCANCODE_7            0x08
+#define SCANCODE_8            0x09
+#define SCANCODE_9            0x0A
+#define SCANCODE_0            0x0B
+#define SCANCODE_MINUS        0x0C
+#define SCANCODE_EQUALS       0x0D
+#define SCANCODE_BACKSPACE    0x0E
+#define SCANCODE_TAB          0x0F
+#define SCANCODE_Q            0x10
+#define SCANCODE_W            0x11
+#define SCANCODE_E            0x12
+#define SCANCODE_R            0x13
+#define SCANCODE_T            0x14
+#define SCANCODE_Y            0x15
+#define SCANCODE_U            0x16
+#define SCANCODE_I            0x17
+#define SCANCODE_O            0x18
+#define SCANCODE_P            0x19
+#define SCANCODE_LBRACKET     0x1A
+#define SCANCODE_RBRACKET     0x1B
+#define SCANCODE_ENTER        0x1C
+#define SCANCODE_LCTRL        0x1D
+#define SCANCODE_A            0x1E
+#define SCANCODE_S            0x1F
+#define SCANCODE_D            0x20
+#define SCANCODE_F            0x21
+#define SCANCODE_G            0x22
+#define SCANCODE_H            0x23
+#define SCANCODE_J            0x24
+#define SCANCODE_K            0x25
+#define SCANCODE_L            0x26
+#define SCANCODE_SEMICOLON    0x27
+#define SCANCODE_APOSTROPHE   0x28
+#define SCANCODE_GRAVE        0x29
+#define SCANCODE_LSHIFT       0x2A
+#define SCANCODE_BACKSLASH    0x2B
+#define SCANCODE_Z            0x2C
+#define SCANCODE_X            0x2D
+#define SCANCODE_C            0x2E
+#define SCANCODE_V            0x2F
+#define SCANCODE_B            0x30
+#define SCANCODE_N            0x31
+#define SCANCODE_M            0x32
+#define SCANCODE_COMMA        0x33
+#define SCANCODE_PERIOD       0x34
+#define SCANCODE_SLASH        0x35
+#define SCANCODE_RSHIFT       0x36
+#define SCANCODE_KP_ASTERISK  0x37
+#define SCANCODE_LALT         0x38
+#define SCANCODE_SPACE        0x39
+#define SCANCODE_CAPSLOCK     0x3A
+#define SCANCODE_F1           0x3B
+#define SCANCODE_F2           0x3C
+#define SCANCODE_F3           0x3D
+#define SCANCODE_F4           0x3E
+#define SCANCODE_F5           0x3F
+#define SCANCODE_F6           0x40
+#define SCANCODE_F7           0x41
+#define SCANCODE_F8           0x42
+#define SCANCODE_F9           0x43
+#define SCANCODE_F10          0x44
+#define SCANCODE_NUMLOCK      0x45
+#define SCANCODE_SCROLLLOCK   0x46
+#define SCANCODE_KP_7         0x47
+#define SCANCODE_KP_8         0x48
+#define SCANCODE_KP_9         0x49
+#define SCANCODE_KP_MINUS     0x4A
+#define SCANCODE_KP_4         0x4B
+#define SCANCODE_KP_5         0x4C
+#define SCANCODE_KP_6         0x4D
+#define SCANCODE_KP_PLUS      0x4E
+#define SCANCODE_KP_1         0x4F
+#define SCANCODE_KP_2         0x50
+#define SCANCODE_KP_3         0x51
+#define SCANCODE_KP_0         0x52
+#define SCANCODE_KP_PERIOD    0x53
+#define SCANCODE_F11          0x57
+#define SCANCODE_F12          0x58
+
+
+// Extended scan codes
+#define SCANCODE_RCTRL        0xE01D
+#define SCANCODE_RALT         0xE038
+#define SCANCODE_HOME         0xE047
+#define SCANCODE_UP           0xE048
+#define SCANCODE_PAGEUP       0xE049
+#define SCANCODE_LEFT         0xE04B
+#define SCANCODE_RIGHT        0xE04D
+#define SCANCODE_END          0xE04F
+#define SCANCODE_DOWN         0xE050
+#define SCANCODE_PAGEDOWN     0xE051
+#define SCANCODE_INSERT       0xE052
+#define SCANCODE_DELETE       0xE053
+
+
+
+// Get the break code for a specific scan code
+#define BREAK_CODE(scanCode)  ((scanCode) + 0x80)
 
 
 
 // Abstract key codes
-typedef enum {
-    KeyCodeNone          = 0x00,
-    KeyCodeError         = 0x01,
-    KeyCodeReserved      = 0x02,
-    KeyCodeSystem        = 0x03,
-    KeyCodeA             = 0x04,
-    KeyCodeB             = 0x05,
-    KeyCodeC             = 0x06,
-    KeyCodeD             = 0x07,
-    KeyCodeE             = 0x08,
-    KeyCodeF             = 0x09,
-    KeyCodeG             = 0x0A,
-    KeyCodeH             = 0x0B,
-    KeyCodeI             = 0x0C,
-    KeyCodeJ             = 0x0D,
-    KeyCodeK             = 0x0E,
-    KeyCodeL             = 0x0F,
-    KeyCodeM             = 0x10,
-    KeyCodeN             = 0x11,
-    KeyCodeO             = 0x12,
-    KeyCodeP             = 0x13,
-    KeyCodeQ             = 0x14,
-    KeyCodeR             = 0x15,
-    KeyCodeS             = 0x16,
-    KeyCodeT             = 0x17,
-    KeyCodeU             = 0x18,
-    KeyCodeV             = 0x19,
-    KeyCodeW             = 0x1A,
-    KeyCodeX             = 0x1B,
-    KeyCodeY             = 0x1C,
-    KeyCodeZ             = 0x1D,
-    KeyCode1             = 0x1E,
-    KeyCode2             = 0x1F,
-    KeyCode3             = 0x20,
-    KeyCode4             = 0x21,
-    KeyCode5             = 0x22,
-    KeyCode6             = 0x23,
-    KeyCode7             = 0x24,
-    KeyCode8             = 0x25,
-    KeyCode9             = 0x26,
-    KeyCode0             = 0x27,
-    KeyCodeEnter         = 0x28,
-    KeyCodeEsc           = 0x29,
-    KeyCodeBackspace     = 0x2A,
-    KeyCodeTab           = 0x2B,
-    KeyCodeSpace         = 0x2C,
-    KeyCodeMinus         = 0x2D,
-    KeyCodeEquals        = 0x2E,
-    KeyCodeLBracket      = 0x2F,
-    KeyCodeRBracket      = 0x30,
-    KeyCodeBackslash     = 0x31,
-    KeyCodeSemicolon     = 0x32,
-    KeyCodeApostrophe    = 0x33,
-    KeyCodeGrave         = 0x34,
-    KeyCodeComma         = 0x35,
-    KeyCodePeriod        = 0x36,
-    KeyCodeSlash         = 0x37,
-    KeyCodeCapsLock      = 0x38,
-    KeyCodeF1            = 0x39,
-    KeyCodeF2            = 0x3A,
-    KeyCodeF3            = 0x3B,
-    KeyCodeF4            = 0x3C,
-    KeyCodeF5            = 0x3D,
-    KeyCodeF6            = 0x3E,
-    KeyCodeF7            = 0x3F,
-    KeyCodeF8            = 0x40,
-    KeyCodeF9            = 0x41,
-    KeyCodeF10           = 0x42,
-    KeyCodeF11           = 0x43,
-    KeyCodeF12           = 0x44,
-    KeyCodePrintScreen   = 0x45,
-    KeyCodeScrollLock    = 0x46,
-    KeyCodePause         = 0x47,
-    KeyCodeInsert        = 0x48,
-    KeyCodeHome          = 0x49,
-    KeyCodePageUp        = 0x4A,
-    KeyCodeDelete        = 0x4B,
-    KeyCodeEnd           = 0x4C,
-    KeyCodePageDown      = 0x4D,
-    KeyCodeRight         = 0x4E,
-    KeyCodeLeft          = 0x4F,
-    KeyCodeDown          = 0x50,
-    KeyCodeUp            = 0x51,
-    KeyCodeNumLock       = 0x52,
-    KeyCodeKpSlash       = 0x53,
-    KeyCodeKpAsterisk    = 0x54,
-    KeyCodeKpMinus       = 0x55,
-    KeyCodeKpPlus        = 0x56,
-    KeyCodeKpEnter       = 0x57,
-    KeyCodeKp1           = 0x58,
-    KeyCodeKp2           = 0x59,
-    KeyCodeKp3           = 0x5A,
-    KeyCodeKp4           = 0x5B,
-    KeyCodeKp5           = 0x5C,
-    KeyCodeKp6           = 0x5D,
-    KeyCodeKp7           = 0x5E,
-    KeyCodeKp8           = 0x5F,
-    KeyCodeKp9           = 0x60,
-    KeyCodeKp0           = 0x61,
-    KeyCodeKpPeriod      = 0x62,
-    KeyCodeLCtrl         = 0x63,
-    KeyCodeLShift        = 0x64,
-    KeyCodeLAlt          = 0x65,
-    KeyCodeLGui          = 0x66,
-    KeyCodeRCtrl         = 0x67,
-    KeyCodeRShift        = 0x68,
-    KeyCodeRAlt          = 0x69,
-    KeyCodeRGui          = 0x6A
-} KeyCode;
+#define KEY_NONE          0x00
+#define KEY_ERROR         0x01
+#define KEY_RESERVED      0x02
+#define KEY_SYSTEM        0x03
+#define KEY_A             0x04
+#define KEY_B             0x05
+#define KEY_C             0x06
+#define KEY_D             0x07
+#define KEY_E             0x08
+#define KEY_F             0x09
+#define KEY_G             0x0A
+#define KEY_H             0x0B
+#define KEY_I             0x0C
+#define KEY_J             0x0D
+#define KEY_K             0x0E
+#define KEY_L             0x0F
+#define KEY_M             0x10
+#define KEY_N             0x11
+#define KEY_O             0x12
+#define KEY_P             0x13
+#define KEY_Q             0x14
+#define KEY_R             0x15
+#define KEY_S             0x16
+#define KEY_T             0x17
+#define KEY_U             0x18
+#define KEY_V             0x19
+#define KEY_W             0x1A
+#define KEY_X             0x1B
+#define KEY_Y             0x1C
+#define KEY_Z             0x1D
+#define KEY_1             0x1E
+#define KEY_2             0x1F
+#define KEY_3             0x20
+#define KEY_4             0x21
+#define KEY_5             0x22
+#define KEY_6             0x23
+#define KEY_7             0x24
+#define KEY_8             0x25
+#define KEY_9             0x26
+#define KEY_0             0x27
+#define KEY_ENTER         0x28
+#define KEY_ESC           0x29
+#define KEY_BACKSPACE     0x2A
+#define KEY_TAB           0x2B
+#define KEY_SPACE         0x2C
+#define KEY_MINUS         0x2D
+#define KEY_EQUALS        0x2E
+#define KEY_LBRACKET      0x2F
+#define KEY_RBRACKET      0x30
+#define KEY_BACKSLASH     0x31
+#define KEY_SEMICOLON     0x32
+#define KEY_APOSTROPHE    0x33
+#define KEY_GRAVE         0x34
+#define KEY_COMMA         0x35
+#define KEY_PERIOD        0x36
+#define KEY_SLASH         0x37
+#define KEY_CAPSLOCK      0x38
+#define KEY_F1            0x39
+#define KEY_F2            0x3A
+#define KEY_F3            0x3B
+#define KEY_F4            0x3C
+#define KEY_F5            0x3D
+#define KEY_F6            0x3E
+#define KEY_F7            0x3F
+#define KEY_F8            0x40
+#define KEY_F9            0x41
+#define KEY_F10           0x42
+#define KEY_F11           0x43
+#define KEY_F12           0x44
+#define KEY_PRINTSCREEN   0x45
+#define KEY_SCROLLLOCK    0x46
+#define KEY_PAUSE         0x47
+#define KEY_INSERT        0x48
+#define KEY_HOME          0x49
+#define KEY_PAGEUP        0x4A
+#define KEY_DELETE        0x4B
+#define KEY_END           0x4C
+#define KEY_PAGEDOWN      0x4D
+#define KEY_RIGHT         0x4E
+#define KEY_LEFT          0x4F
+#define KEY_DOWN          0x50
+#define KEY_UP            0x51
+#define KEY_NUMLOCK       0x52
+#define KEY_KP_SLASH      0x53
+#define KEY_KP_ASTERISK   0x54
+#define KEY_KP_MINUS      0x55
+#define KEY_KP_PLUS       0x56
+#define KEY_KP_ENTER      0x57
+#define KEY_KP_1          0x58
+#define KEY_KP_2          0x59
+#define KEY_KP_3          0x5A
+#define KEY_KP_4          0x5B
+#define KEY_KP_5          0x5C
+#define KEY_KP_6          0x5D
+#define KEY_KP_7          0x5E
+#define KEY_KP_8          0x5F
+#define KEY_KP_9          0x60
+#define KEY_KP_0          0x61
+#define KEY_KP_PERIOD     0x62
+#define KEY_LCTRL         0x63
+#define KEY_LSHIFT        0x64
+#define KEY_LALT          0x65
+#define KEY_LGUI          0x66
+#define KEY_RCTRL         0x67
+#define KEY_RSHIFT        0x68
+#define KEY_RALT          0x69
+#define KEY_RGUI          0x6A
 
 
+module(Keyboard) {
+  U16 (*ReadScanCode)(void);
+  KeyCode (*GetKeyCode)(U16 scanCode);
+  void (*UpdateModifiers)(KeyCode keyCode, bool keyDown, KeyModifiers *modifiers);
+  char (*GetChar)(KeyCode keyCode, KeyModifiers modifiers);
+};
 
 
 
--- Source/Kernel/Kernel.Main.c
+++ Source/Kernel/Kernel.Main.c
@@ -38,6 +38,7 @@
 use(Bitmap);
 use(Heap);
 use(String);
+use(Keyboard);
 
 extern U16 _Kernel_LowMemoryInfoKiB;
 
@@ -64,133 +65,25 @@ static IdtDescriptor _Kernel_IdtDescriptor;
 /* extern void IrqKeyboard_Stub(void); */
 
 static U16 pixelX = 0;
-ScanCode scanCode = 0;
+
 static U8 extended = 0;
+static KeyModifiers  _KeyModifiers;
+static KeyEventArgs _KeyArgs;
 static void KeyboardHandler(void) {
-      U8 raw = PortReadByte(0x60);
+  U16 scanCode = Keyboard.ReadScanCode();
+  if (!scanCode)
+    return;
 
-    if (raw == 0xe0 || raw == 0xe1) {
-        extended = raw;
-        return;
-    }
+  bool wasKeyDown = (scanCode & 0xFF) & 0x80;
+  U8 keyCode = Keyboard.GetKeyCode(wasKeyDown ? (scanCode - 0x80) : scanCode);
+  Keyboard.UpdateModifiers(keyCode, wasKeyDown, &_KeyModifiers);
 
-    bool isRelease = (raw & 0x80) != 0;
-    U16 code = raw & 0x7f;
-
-    if (extended) {
-        code |= (extended << 8);
-        extended = 0;
-    }
-
-    scanCode = (ScanCode)code;
-
-    if (!isRelease) {
-        pixelX++;
-    }
+  _KeyArgs = (KeyEventArgs) {
+    .KeyCode = keyCode,
+    .WasKeyPress = wasKeyDown,
+    .Modifiers = &_KeyModifiers
+  };
 }
-
-
-KeyCode TranslateScanCode(ScanCode scanCode) {
-    switch (scanCode) {
-        /* Letters */
-        case ScanCodeA: return KeyCodeA;
-        case ScanCodeB: return KeyCodeB;
-        case ScanCodeC: return KeyCodeC;
-        case ScanCodeD: return KeyCodeD;
-        case ScanCodeE: return KeyCodeE;
-        case ScanCodeF: return KeyCodeF;
-        case ScanCodeG: return KeyCodeG;
-        case ScanCodeH: return KeyCodeH;
-        case ScanCodeI: return KeyCodeI;
-        case ScanCodeJ: return KeyCodeJ;
-        case ScanCodeK: return KeyCodeK;
-        case ScanCodeL: return KeyCodeL;
-        case ScanCodeM: return KeyCodeM;
-        case ScanCodeN: return KeyCodeN;
-        case ScanCodeO: return KeyCodeO;
-        case ScanCodeP: return KeyCodeP;
-        case ScanCodeQ: return KeyCodeQ;
-        case ScanCodeR: return KeyCodeR;
-        case ScanCodeS: return KeyCodeS;
-        case ScanCodeT: return KeyCodeT;
-        case ScanCodeU: return KeyCodeU;
-        case ScanCodeV: return KeyCodeV;
-        case ScanCodeW: return KeyCodeW;
-        case ScanCodeX: return KeyCodeX;
-        case ScanCodeY: return KeyCodeY;
-        case ScanCodeZ: return KeyCodeZ;
-
-        /* Numbers */
-        case ScanCode1: return KeyCode1;
-        case ScanCode2: return KeyCode2;
-        case ScanCode3: return KeyCode3;
-        case ScanCode4: return KeyCode4;
-        case ScanCode5: return KeyCode5;
-        case ScanCode6: return KeyCode6;
-        case ScanCode7: return KeyCode7;
-        case ScanCode8: return KeyCode8;
-        case ScanCode9: return KeyCode9;
-        case ScanCode0: return KeyCode0;
-
-        /* Control keys */
-        case ScanCodeEnter:     return KeyCodeEnter;
-        case ScanCodeEsc:       return KeyCodeEsc;
-        case ScanCodeBackspace: return KeyCodeBackspace;
-        case ScanCodeTab:       return KeyCodeTab;
-        case ScanCodeSpace:     return KeyCodeSpace;
-
-        /* Symbols */
-        case ScanCodeMinus:      return KeyCodeMinus;
-        case ScanCodeEquals:     return KeyCodeEquals;
-        case ScanCodeLBracket:   return KeyCodeLBracket;
-        case ScanCodeRBracket:   return KeyCodeRBracket;
-        case ScanCodeBackslash:  return KeyCodeBackslash;
-        case ScanCodeSemicolon:  return KeyCodeSemicolon;
-        case ScanCodeApostrophe: return KeyCodeApostrophe;
-        case ScanCodeGrave:      return KeyCodeGrave;
-        case ScanCodeComma:      return KeyCodeComma;
-        case ScanCodePeriod:     return KeyCodePeriod;
-        case ScanCodeSlash:      return KeyCodeSlash;
-
-        /* Modifiers */
-        case ScanCodeLShift: return KeyCodeLShift;
-        case ScanCodeLCtrl:  return KeyCodeLCtrl;
-        case ScanCodeLAlt:   return KeyCodeLAlt;
-
-        /* Function keys */
-        case ScanCodeF1:  return KeyCodeF1;
-        case ScanCodeF2:  return KeyCodeF2;
-        case ScanCodeF3:  return KeyCodeF3;
-        case ScanCodeF4:  return KeyCodeF4;
-        case ScanCodeF5:  return KeyCodeF5;
-        case ScanCodeF6:  return KeyCodeF6;
-        case ScanCodeF7:  return KeyCodeF7;
-        case ScanCodeF8:  return KeyCodeF8;
-        case ScanCodeF9:  return KeyCodeF9;
-        case ScanCodeF10: return KeyCodeF10;
-        case ScanCodeF11: return KeyCodeF11;
-        case ScanCodeF12: return KeyCodeF12;
-
-        /* Extended keys */
-        case ScanCodeRCtrl:     return KeyCodeRCtrl;
-        case ScanCodeRAlt:      return KeyCodeRAlt;
-        case ScanCodeHome:      return KeyCodeHome;
-        case ScanCodeUp:        return KeyCodeUp;
-        case ScanCodePageUp:    return KeyCodePageUp;
-        case ScanCodeLeft:      return KeyCodeLeft;
-        case ScanCodeRight:     return KeyCodeRight;
-        case ScanCodeEnd:       return KeyCodeEnd;
-        case ScanCodeDown:      return KeyCodeDown;
-        case ScanCodePageDown:  return KeyCodePageDown;
-        case ScanCodeInsert:    return KeyCodeInsert;
-        case ScanCodeDelete:    return KeyCodeDelete;
-
-        default:
-            return KeyCodeError;
-    }
-}
-
-
 
 
 /* Naked attribute: no prolog/epilog */
@@ -286,9 +179,6 @@ void KernelMain() {
     Gfx.Core.RefreshFromBackBuffer();
   
   Gfx.Draw.FilledRect(0, 0, 639, 14, 7);
-
-  
-  char textBuffer[100] = { };
   Gfx.Core.RefreshFromBackBuffer();
     
  
@@ -311,7 +201,7 @@ void KernelMain() {
     Gfx.Draw.String(340, 3, memText, 0);
 
     char scanCodeText[100] = { };
-    String.Format(scanCodeText, "%x", scanCode);
+    //String.Format(scanCodeText, "KeyCode = %d", _KeyArgs.KeyCode & 0xff);
     Gfx.Draw.String(10, 20, scanCodeText, 4);
     Gfx.Core.RefreshFromBackBuffer();
     
